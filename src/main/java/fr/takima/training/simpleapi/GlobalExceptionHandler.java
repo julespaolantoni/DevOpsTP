fbbfe3ab8c1bc78b37d3e658738ac11122c74707
julespaolantoni <jules.paolantoni@gmail.com>
2025-10-21 12:47:02 +0200

    TP1

---
+++ src/main/java/fr/takima/training/simpleapi/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package fr.takima.training.simpleapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        ex.printStackTrace(); // Log the error
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("An error occurred: " + ex.getMessage());
+    }
+}
